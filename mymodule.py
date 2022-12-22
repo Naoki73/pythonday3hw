40d17ddb8615b53cd8e8bf0fe554c7ca7e1f8829
Eric Rincon <royeric77@gmail.com>
2022-12-22 17:03:18 -0600

    my homework

---
+++ mymodule.py
@@ -0,0 +1,10 @@
+def findsqft(length, width):
+    length = int(length)
+    width = int(width)
+    return length * width
+
+
+def findcirc(diameter):
+    diameter = int(diameter)
+    
+    return diameter * 3.14
---
+++ shoha.py
@@ -0,0 +1,4 @@
+def roundUp(num):
+    return int(num)+1
+
+name = "shoha"
